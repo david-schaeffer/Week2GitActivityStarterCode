6b0fc41b9ffc8c745aee2cd07bcd89d0ccec75ce
Christian Bernier <christianbernier02@gmail.com>
2023-09-12 11:15:52 -0400

    implement divide operation

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,8 @@ const result_subtract = calc.subtract(3, 2);
 console.log(`result_subtract = ${result_subtract}`);
 
 const result_multiply = calc.multiply(3, 2);
-console.log(`result_multiply = ${result_multiply}`)
+console.log(`result_multiply = ${result_multiply}`);
+
+const result_divide = calc.divide(22, 7);
+console.log(`result_divide = ${result_divide}`);
+
